49350c3a120a96e0cbbb6ba12c5001200a7c043f
peter88213 <peter.88213@t-online.de>
2023-12-21 01:27:41 +0100

    Prepare v2.2

--- src/cmw.py
+++ src/cmw.py
@@ -1,9 +1,9 @@
-"""Create Markdown-formatted text files from the Manuscript world and characters files.
+"""Create Markdown-formatted text files from the Manuskript world and characters files.
 
 usage: crmd.py [-h] projectdir
 
 positional arguments:
-  projectdir  The Manuscript project directory.
+  projectdir  The Manuskript project directory.
 
 options:
   -h, --help  show this help message and exit
@@ -16,6 +16,7 @@ v1.1: Add "shebang"; refactor.
 v1.2: Fix a typo in the help text.
 v2.0: Change the interface and convert the characters as well.
 v2.1: Catch exceptions separately for characters and world.
+v2.2: Refactor; fix messages.
 
 Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/convert_manuskript_world
@@ -32,7 +33,7 @@ def convert_world(prjDir):
     """Create a Markdown file with the contents of the project's "world.opml" file.
     
     Positional arguments:
-        prjDir: str -- The Manuscript project directory.
+        prjDir: str -- The Manuskript project directory.
     
     Return a message on success. 
     Raise an exception on error.
@@ -51,7 +52,7 @@ def convert_world(prjDir):
     lines = []
     xmlBody = xmlTree.getroot().find('body')
     if xmlBody is None:
-        raise ValueError(f'"{filePath}" seems not to be a Manuscript world file.')
+        raise ValueError(f'"{filePath}" seems not to be a Manuskript world file.')
 
     convert_branch(xmlBody, 0)
     newFile = f'{prjDir}/world.md'
@@ -64,7 +65,7 @@ def convert_characters(prjDir):
     """Create a Markdown file with the contents of project's "characters" files.
     
     Positional arguments:
-        prjDir: str -- The Manuscript project directory.
+        prjDir: str -- The Manuskript project directory.
     
     Return a message on success. 
     Raise an exception on error.
@@ -96,6 +97,7 @@ def convert_characters(prjDir):
                         else:
                             newlines.append(f'## {heading}')
                             newlines.append(text)
+                    break
     newFile = f'{prjDir}/characters.md'
     with open(newFile, 'w', encoding='utf-8') as f:
         f.write('\n\n'.join(newlines))
@@ -104,10 +106,10 @@ def convert_characters(prjDir):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description=f'Create Markdown-formatted text files from the Manuscript world and characters files.',
+        description=f'Create Markdown-formatted text files from the Manuskript world and characters files.',
         epilog='The created text files "world.md" and "characters.md" are placed in the Manuskript project directory.')
     parser.add_argument('prjDir', metavar='projectdir',
-                        help='The Manuscript project directory.')
+                        help='The Manuskript project directory.')
     args = parser.parse_args()
     try:
         print(convert_world(args.prjDir))
